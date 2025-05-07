5bf55a29fb55bcb7ff9f93837443137c95b79eeb
Bartosz Paradowski <bartoszparadowski01@gmail.com>
2025-05-07 11:21:05 +0200

    setup biome and some basic stuff

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -7,10 +7,21 @@
 |
 */
 
-import router from '@adonisjs/core/services/router'
+import router from "@adonisjs/core/services/router";
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+// start/routes.ts
+
+router.ws("/room/:roomId", ({ ws, params, auth }) => {
+	// const roomId = params.roomId
+	// const user = auth.user
+
+	ws.on("message", (message) => {
+		ws.broadcast(`Someone said:  + ${message.toString()}`);
+	});
+
+	ws.on("close", () => {
+		console.log("Connection closed");
+	});
+
+	ws.send(`Hello! Your id is ${ws.id}`);
+});
